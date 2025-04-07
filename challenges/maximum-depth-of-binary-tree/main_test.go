0277f8173a56f48ffb79d15e923542475bcb10ba
Noé Alcantar <oscarnoegarcia@gmail.com>
2025-04-07 10:19:32 -0400

    Maximum Depth of Binary Tree

---
+++ challenges/maximum-depth-of-binary-tree/main_test.go
@@ -0,0 +1,55 @@
+package main
+
+import "testing"
+
+func Test_maxDepth(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "root = [3,9,20,null,null,15,7] | 3",
+			args: args{
+				root: &TreeNode{
+					Val: 3,
+					Left: &TreeNode{
+						Val: 9,
+					},
+					Right: &TreeNode{
+						Val: 20,
+						Left: &TreeNode{
+							Val: 15,
+						},
+						Right: &TreeNode{
+							Val: 7,
+						},
+					},
+				},
+			},
+			want: 3,
+		},
+		{
+			name: "root = [1,null,2] | 2",
+			args: args{
+				root: &TreeNode{
+					Val: 1,
+					Right: &TreeNode{
+						Val: 2,
+					},
+				},
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxDepth(tt.args.root); got != tt.want {
+				t.Errorf("maxDepth() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
